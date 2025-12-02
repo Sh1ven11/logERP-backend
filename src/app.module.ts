f880493ea8eac70b95c4bccb0cd8d104db03fbdd
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-12-02 18:09:43 +0530

    Auth is working now

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { UserController } from './user/user.controller';
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [UserController],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,12 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { LoginDto } from './dto';
 import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma : PrismaService) {}
+    constructor(private prisma : PrismaService, private jwt:JwtService, private config:ConfigService) {}
     async login(body:LoginDto){
         const hash = await argon2.hash(body.password);
         const user =await this.prisma.user.findUnique({
@@ -18,7 +20,19 @@ export class AuthService {
         const pwMatches = await argon2.verify(user.password, body.password);
         if(!pwMatches) throw new ForbiddenException('Incorrect Credentials')
         const { password, ...userWithoutPassword } = user;
-        return { message: 'Login Successful', user: userWithoutPassword }
+        return this.signToken(user.id, user.username);
     
     }   
+    async signToken(userId:number, username:string):Promise<{access_token:string}>{ 
+        const payload={
+            sub:userId,
+            username
+        }
+        const secret=this.config.get('JWT_SECRET');
+        const token = await this.jwt.signAsync(payload,{
+            expiresIn:'15m',
+            secret: secret,
+        })
+        return {access_token:token};
+    }
 }
