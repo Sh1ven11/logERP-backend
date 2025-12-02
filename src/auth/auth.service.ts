ab1bd244ed4b7382df0298e05f1fa427e9d1966e
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-12-02 12:44:59 +0530

    login successful

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { LoginDto } from './dto';
 import * as argon2 from 'argon2';
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma : PrismaService) {   }
+    constructor(private prisma : PrismaService) {}
     async login(body:LoginDto){
         const hash = await argon2.hash(body.password);
+        const user =await this.prisma.user.findUnique({
+            where:{
+                username:body.username,
+            }
+        })
+        if(!user)throw new ForbiddenException('Incorrect Credentials')
         // Implement login logic here
-        return {message: "Login successful", data: body, hash};
+        const pwMatches = await argon2.verify(user.password, body.password);
+        if(!pwMatches) throw new ForbiddenException('Incorrect Credentials')
+        const { password, ...userWithoutPassword } = user;
+        return { message: 'Login Successful', user: userWithoutPassword }
+    
     }   
 }
