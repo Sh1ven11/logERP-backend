4485bc87de1938c7abd8fa3790acfb8f2c645740
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-12-03 13:04:20 +0530

    Customer feature working

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
-
+import { CustomerController } from './customer.controller';
 @Module({
+  controllers: [CustomerController],
   providers: [CustomerService]
 })
-export class ClientsModule {}
+export class CustomerModule {}
